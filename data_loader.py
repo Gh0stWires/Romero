abc2be0816cd534fd33a5d7fae617fa0ae22f4f2
Sam Grogan <sam.grogan@bottlerocketstudios.com>
2021-11-30 16:22:52 -0600

    add loop for resizing the images

--- data_loader.py
+++ data_loader.py
@@ -2,6 +2,7 @@ import pathlib
 import numpy as np
 import PIL
 import tensorflow as tf
+from pathlib import Path
 
 debug = True
 
@@ -10,7 +11,8 @@ def pre_process(directory):
     data_dir = pathlib.Path(directory)
     files = list(data_dir.glob('*wallmap.png'))
     print(f"Image count: {len(files)}")
-    process_image(files[0])
+    for file in files:
+        process_image(file)
 
 
 def process_image(image):
@@ -20,10 +22,17 @@ def process_image(image):
     print(f"Image shape: {img_array.shape}") if debug else None
     img_array = img_array.reshape(img_array.shape[0], img_array.shape[1], 1)
     print(f"Image shape: {img_array.shape}") if debug else None
-    tf_img = tf.image.resize_with_crop_or_pad(img_array, 128, 128)
+    tf_img = tf.image.resize_with_crop_or_pad(img_array, target_height=128, target_width=128)
     print(F"Image tensor shape: {tf_img.shape}") if debug else None
-    out_array = tf_img.numpy
+    out_array = tf_img.numpy()
     print(f"Output shape: {out_array.shape}") if debug else None
+
+    test = np.squeeze(out_array, axis=2)
+
+    im = PIL.Image.fromarray(test * 255)
+    im = im.convert("L")
+    im.save(Path('/Users/sam.grogan/Documents/Romero/processed-images/' + image.name))
+
 
 
 #     TODO - load image.
