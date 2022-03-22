dc49535c87d7bf7e5721d65622d3803f9532e9f8
Sam <gh0stmusc@gmail.com>
2022-03-22 15:50:45 -0500

    constraint clipping

--- Wdoomgan.py
+++ Wdoomgan.py
@@ -49,7 +49,11 @@ seed = tf.random.normal([num_examples_to_generate, noise_dim])
 # Load image data
 data = tf.keras.utils.image_dataset_from_directory(
     envirment_utils.processed_directory, labels=None, label_mode=None,
-    class_names=None, color_mode='grayscale', batch_size=BATCH_SIZE, image_size=(128, 128))
+    class_names=None, color_mode='grayscale', batch_size=None, image_size=(128, 128), shuffle=True)
+#print(data.shape)
+
+data = data.batch(BATCH_SIZE, drop_remainder=True)
+#print(data.shape)
 
 
 # Notice the use of `tf.function`
@@ -61,11 +65,11 @@ def train_step(images):
     with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
         generated_images = generator(noise, training=True)
         #moi_z_images = model_utils.add_label_noise(generated_images, 0)
-        real_output = discriminator(generated_images, training=True)
+        real_output = discriminator(images, training=True)
         fake_output = discriminator(generated_images, training=True)
 
         gen_loss = w_generator_loss(fake_output)
-        disc_loss = w_discriminator_loss(real_output, fake_output)
+        disc_loss = w_discriminator_loss(real_output, fake_output, discriminator, images, generated_images)
 
     gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
     gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
